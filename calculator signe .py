da5e0a5f952f0374874c005e7b51ecf53e8874e8
Ajit kumar <kumarajit19009@gmail.com>
2026-02-17 15:09:00 +0530

    add calculator signe.py

---
+++ calculator signe .py
@@ -0,0 +1,27 @@
+num1 = float(input("enter first number:"))
+num2 =float(input("enter secand number:"))
+operator = input("enter operator(+,-,*,/):")
+if operator == '+':
+  result =num1+num2
+  print(f"result={result}")
+elif operator=='-':
+  result=num1-num2
+  print(f"result={result}")
+elif operator =='*':
+  result=num1*num2
+  print(f"result={result}")
+elif operator=='/':
+  if num2 ==0:
+    print('error:division by zero')
+  else:
+    result=num1/num2
+    print(f"result={result}")
+else:
+  print("invalid operator")
+ # RESULT CHEKE
+if result>0:
+  print("positive")
+elif result<0:
+  print("negative")
+else:
+  print("zero")
